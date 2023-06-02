d28b8f98a1efa41a1af35a772cbe87dfba437e7a
bimoso <elnonino23@email.com>
2023-06-01 19:58:01 -0600

    Listo

--- script/script.js
+++ script/script.js
@@ -1,10 +1,45 @@
-const number = getElementById('numero');
-const contador = getElementById('contador');
+// Obtén los elementos del DOM
+var contadorElemento = document.getElementById("contador");
+var inputNumero = document.getElementById("numero");
+var btnAñadir = document.getElementById("btnAñadir");
+var btnQuitar = document.getElementById("btnQuitar");
+var btnIncrementar = document.getElementById("btnIncrementar");
+var btnDecrementar = document.getElementById("btnDecrementar");
+var btnReset = document.getElementById("btnReset");
 
-function addNumber() {
-    let numero = number.value;
-    contador.innerHTML = numero;
-    return 
+// Inicializa el contador
+var contador = 0;
+actualizarContador();
+
+// Agrega event listeners a los botones
+btnAñadir.addEventListener("click", function () {
+    let numero = parseInt(inputNumero.value);
+    contador += numero;
+    actualizarContador();
+});
+
+btnQuitar.addEventListener("click", function () {
+    let numero = parseInt(inputNumero.value);
+    contador -= numero;
+    actualizarContador();
+});
+
+btnIncrementar.addEventListener("click", function () {
+    contador++;
+    actualizarContador();
+});
+
+btnDecrementar.addEventListener("click", function () {
+    contador--;
+    actualizarContador();
+});
+
+btnReset.addEventListener("click", function () {
+    contador = 0;
+    actualizarContador();
+});
+
+// Función para actualizar el contenido del contador en el DOM
+function actualizarContador() {
+    contadorElemento.innerText = contador.toString();
 }
-
-addNumber();
